8444d0fb213828e0bde93d9c749e44e3f27f3479
XavierR007 <christianxavier117@outlook.com>
2023-03-04 12:18:54 -0600

    proyecto git

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PROYECTO3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa un Numero Porfavor");
+            while (true)
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+
+                if (num == -1)
+                    break;
+
+                if (num % 2 == 0)
+                    Console.WriteLine("Tu Numero es Par");
+                else
+                    Console.WriteLine("Tu Numero es Impar");
+
+            }
+        }
+    }
+}
